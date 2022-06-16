607f044dc496b2cbd37ab15179a620ea2f0b3f16
Cole Friedlaender <75972900+colefriedlaender@users.noreply.github.com>
2022-06-16 22:03:19 +0200

    Update ReservationSystemApplication.java

--- src/main/java/com/application/reservationsystem/ReservationSystemApplication.java
+++ src/main/java/com/application/reservationsystem/ReservationSystemApplication.java
@@ -3,7 +3,7 @@ package com.application.reservationsystem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class ReservationSystemApplication {
 
 	public static void main(String[] args) {
